69f19bbaac945bf6b3159b539dcf513146668a7b
lucas <ljeronimodarocha@gmail.com>
2024-03-13 15:47:50 -0300

    Máximo desempenho de 58k em menos de 800ms.

--- stress-test/run-test.sh
+++ stress-test/run-test.sh
@@ -7,7 +7,7 @@
 #GATLING_BIN_DIR=/home/lucas/.local/gatling/bin
 GATLING_BIN_DIR=/usr/local/sbin/gatling/bin
 
-WORKSPACE=/media/lucas/volume1/desenvolvimento/rinha.backend/stress-test
+WORKSPACE=/media/lucas/volume1/desenvolvimento/rinha.backend2/stress-test
 
 sh $GATLING_BIN_DIR/gatling.sh -rm local -s RinhaBackendSimulation \
     -rd "DESCRICAO" \
